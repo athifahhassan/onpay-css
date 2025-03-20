78f24e633e459d4170a5ad75f0b2cb8a6b38a78b
athifahhassan <95356737+athifahhassan@users.noreply.github.com>
2025-03-20 17:19:55 +0800

    Update Slider.js

--- Slider.js
+++ Slider.js
@@ -1,31 +1,30 @@
-<script>
-  var slideIndex = 0;
-  var slideInterval;
+var slideIndex = 0;
+var slideInterval;
 
-  function plusDivs(n, isManual = false) {
-    if (isManual) resetAutoSlide();
-    showDivs(slideIndex += n);
+window.plusDivs = function (n, isManual = false) {
+  if (isManual) resetAutoSlide();
+  showDivs(slideIndex += n);
+};
+
+function showDivs(n) {
+  var slides = document.getElementsByClassName("mySlides");
+  if (n >= slides.length) { slideIndex = 0; }
+  if (n < 0) { slideIndex = slides.length - 1; }
+  
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active-slide");
   }
+  slides[slideIndex].classList.add("active-slide");
+}
 
-  function showDivs(n) {
-    var slides = document.getElementsByClassName("mySlides");
-    if (n >= slides.length) { slideIndex = 0; }
-    if (n < 0) { slideIndex = slides.length - 1; }
-    
-    for (let i = 0; i < slides.length; i++) {
-      slides[i].classList.remove("active-slide");
-    }
-    slides[slideIndex].classList.add("active-slide");
-  }
+function autoSlide() {
+  plusDivs(1);
+  slideInterval = setTimeout(autoSlide, 5000); // Auto-slide every 5s
+}
 
-  function autoSlide() {
-    plusDivs(1);
-    slideInterval = setTimeout(autoSlide, 5000); // Auto-slide every 5s
-  }
+function resetAutoSlide() {
+  clearTimeout(slideInterval);
+  slideInterval = setTimeout(autoSlide, 5000);
+}
 
-  function resetAutoSlide() {
-    clearTimeout(slideInterval);
-    slideInterval = setTimeout(autoSlide, 5000);
-  }
-
-  autoSlide(); // Start auto-sliding
+autoSlide(); // Start auto-sliding
