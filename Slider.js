d0080c0dba95877aad41dd619cff1ffff4b36707
athifahhassan <95356737+athifahhassan@users.noreply.github.com>
2025-03-20 17:17:15 +0800

    Update Slider.js

--- Slider.js
+++ Slider.js
@@ -1,30 +1,31 @@
-var slideIndex = 0;
-	var slideInterval;
-	
-	function plusDivs(n, isManual = false) {
-	  if (isManual) resetAutoSlide();
-	  showDivs(slideIndex += n);
-	}
-	
-	function showDivs(n) {
-	  var slides = document.getElementsByClassName("mySlides");
-	  if (n &gt;= slides.length) { slideIndex = 0; }
-	  if (n &lt; 0) { slideIndex = slides.length - 1; }
-	  
-	  for (let i = 0; i &lt; slides.length; i++) {
-	    slides[i].classList.remove("active-slide");
-	  }
-	  slides[slideIndex].classList.add("active-slide");
-	}
-	
-	function autoSlide() {
-	  plusDivs(1);
-	  slideInterval = setTimeout(autoSlide, 5000); // Auto-slide every 5s
-	}
-	
-	function resetAutoSlide() {
-	  clearTimeout(slideInterval);
-	  slideInterval = setTimeout(autoSlide, 5000);
-	}
-	
-	autoSlide(); // Start auto-sliding
+<script>
+  var slideIndex = 0;
+  var slideInterval;
+
+  function plusDivs(n, isManual = false) {
+    if (isManual) resetAutoSlide();
+    showDivs(slideIndex += n);
+  }
+
+  function showDivs(n) {
+    var slides = document.getElementsByClassName("mySlides");
+    if (n >= slides.length) { slideIndex = 0; }
+    if (n < 0) { slideIndex = slides.length - 1; }
+    
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].classList.remove("active-slide");
+    }
+    slides[slideIndex].classList.add("active-slide");
+  }
+
+  function autoSlide() {
+    plusDivs(1);
+    slideInterval = setTimeout(autoSlide, 5000); // Auto-slide every 5s
+  }
+
+  function resetAutoSlide() {
+    clearTimeout(slideInterval);
+    slideInterval = setTimeout(autoSlide, 5000);
+  }
+
+  autoSlide(); // Start auto-sliding
