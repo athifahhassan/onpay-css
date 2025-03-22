c9ac17d5e30ae64dfc3d1bff70354b81cdeed810
athifahhassan <95356737+athifahhassan@users.noreply.github.com>
2025-03-22 16:25:25 +0800

    Update Subsale.js

--- Subsale.js
+++ Subsale.js
@@ -1,11 +1,13 @@
 const properties = [
-    { img: "https://afrproperty.onpay.my/media/uploads/SetiaIndah5.jpg", price: "RM 1,600,000", address: "Jalan Setia 5 @ Taman Setia Indah", details: "6 Beds | 5 Baths | 6100 SQFT", link: "https://afrproperty.onpay.my/order/form/qila-subsaledetail2" },
+    { img: "https://afrproperty.onpay.my/media/uploads/SetiaIndah5.jpg", price: "RM 1,600,000", address: "Jalan Setia 5 @ Taman Setia Indah", details: "6 Beds | 5 Baths | 6,100 SQFT", link: "https://afrproperty.onpay.my/order/form/qila-subsaledetail2" },
     { img: "https://afrproperty.onpay.my/media/uploads/SetiaTropika.jpg", price: "RM 1,200,000", address: "Dictyo Alba Setia Tropika", details: "4 Beds | 3 Baths | 3,509 SQFT", link: "https://afrproperty.onpay.my/order/form/detailsubsale1" },
     { img: "https://afrproperty.onpay.my/media/uploads/FairwaysPulaiPerdana.jpg", price: "RM 880,000", address: "Fairways @ Sri Pulai Perdana", details: "5 Beds | 4 Baths | 8,600 SQFT", link: "https://afrproperty.onpay.my/order/form/detailsubsale1" },
-    { img: "https://afrproperty.onpay.my/media/uploads/Rumah1.jpg", price: "Price Upon Request", address: "281 Park Ave South, New York, NY 10010", details: "0 Beds | 42,500 SQFT", link: "https://afrproperty.onpay.my/order/form/detailsubsale1" },
-    { img: "https://afrproperty.onpay.my/media/uploads/Rumah2.jpg", price: "$75,000,000", address: "56 North Moore St, New York City, NY 10013", details: "50 Beds", link: "https://afrproperty.onpay.my/order/form/detailsubsale1" },
-    { img: "https://afrproperty.onpay.my/media/uploads/Rumah3.jpg", price: "$69,995,000", address: "165 SURFSIDE DR, Water Mill, NY 11976", details: "8 Beds | 8 Full Baths | 1 Half Bath | 8,600 SQFT", link: "https://afrproperty.onpay.my/order/form/detailsubsale1" },
+    { img: "https://afrproperty.onpay.my/media/uploads/DesaPalma1.jpg", price: "RM 3,000,000", address: "Desa Palma @ Nasa City", details: "6 Beds | 6 Baths | 3,200 SQFT", link: "https://afrproperty.onpay.my/order/form/detailsubsale1" },
+    { img: "https://afrproperty.onpay.my/media/uploads/CountryGardenCentralPark1.jpg", price: "RM 850,000", address: "Laman Harmoni @ Country Garden Central Park", details: "4 Beds | 4 Baths | 2,083 SQFT", link: "https://afrproperty.onpay.my/order/form/detailsubsale1" },
+    { img: "https://afrproperty.onpay.my/media/uploads/TamanSetiaIndahPresint12-1.jpg", price: "RM 750,000", address: "Taman Setia Indah Precinct 12", details: "4 Beds | 3 Baths | 1 Storeroom | 1,300 SQFT", link: "https://afrproperty.onpay.my/order/form/detailsubsale1" },
     { img: "https://afrproperty.onpay.my/media/uploads/SetiaIndah5.jpg", price: "RM 1,600,000", address: "Jalan Setia 5 @ Taman Setia Indah", details: "6 Beds | 5 Baths | 6100 SQFT", link: "https://afrproperty.onpay.my/order/form/detailsubsale1" },
+    { img: "https://afrproperty.onpay.my/media/uploads/Rumah1.jpg", price: "RM 3,000,000", address: "Desa Palma @ Nasa City", details: "6 Beds | 6 Baths | 3,200 SQFT", link: "https://afrproperty.onpay.my/order/form/detailsubsale1" },
+    { img: "https://afrproperty.onpay.my/media/uploads/Rumah2.jpg", price: "RM 850,000", address: "Laman Harmoni @ Country Garden Central Park", details: "4 Beds | 4 Baths | 2,083 SQFT", link: "https://afrproperty.onpay.my/order/form/detailsubsale1" },
     { img: "https://afrproperty.onpay.my/media/uploads/SetiaTropika.jpg", price: "RM 1,200,000", address: "Dictyo Alba Setia Tropika", details: "4 Beds | 3 Baths | 3,509 SQFT", link: "https://afrproperty.onpay.my/order/form/detailsubsale1" }
 ];
 
